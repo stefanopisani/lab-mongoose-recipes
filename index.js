b4b95c91722ba393527f260554be17fc6b2299d8
Stefano Pisani <stefano.pisani92@gmail.com>
2021-04-15 12:40:46 +0100

    mongoose-recipes completed

--- index.js
+++ index.js
@@ -19,9 +19,54 @@ mongoose
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
-  .then(() => {
+  .then((res) => {
     // Run your code here, after you have insured that the connection was made
+    updateDB();
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+async function updateDB() {
+  try {
+    // #2
+    let recipe = await Recipe.create({
+      title: "Pasta al Pesto",
+      level: "Amateur Chef",
+      ingredients: [
+        "Pasta",
+        "Pesto"
+      ],
+      cuisine: "Italian",
+      dishType: "main_course",
+      duration: 20,
+      creator: "Chef SP"
+    });
+    console.log(recipe.title);
+    //#3
+    await Recipe.insertMany(data);
+    console.log('Recipes inserted!');
+    //#4
+    const filter = {
+      title: "Rigatoni alla Genovese"
+    };
+    const update = {
+      duration: 100
+    };
+    let doc = await Recipe.updateMany(filter, update);
+
+    console.log(`${filter.title} updated!`);
+    // #5
+    await Recipe.deleteOne({
+      title: 'Carrot Cake'
+    });
+    console.log('Recipe Deleted');
+
+  } catch (error) {
+    console.log(error);
+  } finally {
+    mongoose.connection.close();
+  }
+}
